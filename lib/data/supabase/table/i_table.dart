93b7cffe846c9e45769eb5277e2301492e6fd5d6
Ahmed Magdy <ahmedmagdymohamedy@gmail.com>
2026-02-24 02:33:55 +0200

    enhance select function in ITable

--- lib/data/supabase/table/i_table.dart
+++ lib/data/supabase/table/i_table.dart
@@ -1,6 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:zakio/data/supabase/db_logger_manager.dart';
 import 'package:zakio/data/supabase/supabase_manager.dart';
+import 'package:zakio/data/supabase/table/db_controllers/db_column_value.dart';
+import 'package:zakio/data/supabase/table/db_controllers/db_range_values.dart';
 
 abstract class TableColumn {
   final String columnName;
@@ -17,21 +19,93 @@ abstract class ITable<T, C extends TableColumn> {
 
   T toModel(Map<String, dynamic> map);
 
-  Future<List<Map<String, dynamic>>> onQuery(PostgrestFilterBuilder Function() queryCall, {String operation = ""}) async {
-    final result = await queryCall();
+  Future<dynamic> onQuery(dynamic queryCall, {String operation = ""}) async {
+    final result = await queryCall;
     DBLoggerManager.logResponse(operation, tableName, result);
     return result;
   }
 
   Future<List<T>> select({
     List<C>? column,
+    DBColumnValue? isEqual,
+    DBColumnValue? isNotEqual,
+    DBColumnValue? greaterThan,
+    DBColumnValue? greaterThanOrEqual,
+    DBColumnValue? lessThan,
+    DBColumnValue? lessThanOrEqual,
+    int? limit = 20,
+    C? orderBy,
+    DbRangeValues? range,
+    bool isSinlge = false,
   }) async {
-    final result = await onQuery(
-      () => SupabaseManager.supabase
-          .from(tableName) //
-          .select(formateColumnToSelections(column)),
-      operation: 'SELECT',
-    );
+    // check if developer selected single item but not provided isEqual condition
+    assert (!isSinlge || isEqual != null);
+
+    dynamic query = SupabaseManager.supabase
+        .from(tableName) //
+        .select(formateColumnToSelections(column));
+
+    // check if there is Equal condition
+    if (isEqual != null) {
+      query = query.eq(isEqual.columnNames, isEqual.value);
+    }
+
+    // check if there is not Equal condition
+    if (isNotEqual != null) {
+      query = query.neq(isNotEqual.columnNames, isNotEqual.value);
+    }
+
+    // check if there is greater than condition
+    if (greaterThan != null) {
+      query = query.gt(greaterThan.columnNames, greaterThan.value);
+    }
+
+    // check if there is greater than or equal condition
+    if (greaterThanOrEqual != null) {
+      query = query.gte(greaterThanOrEqual.columnNames, greaterThanOrEqual.value);
+    }
+
+    // check if there is less than condition
+    if (lessThan != null) {
+      query = query.lt(lessThan.columnNames, lessThan.value);
+    }
+
+    // check if there is less than or equal condition
+    if (lessThanOrEqual != null) {
+      query = query.lte(lessThanOrEqual.columnNames, lessThanOrEqual.value);
+    }
+
+    // check if there is limit
+    if (limit != null) {
+      query = query.limit(limit);
+    }
+
+    // check if there is order by
+    if (orderBy != null) {
+      query = query.order(orderBy.columnName);
+    }
+
+    // check if there is range
+    if (range != null) {
+      query = query.range(range.from, range.to);
+    }
+
+    // check if there is single
+    if (isSinlge) {
+      query = query.single();
+    }
+
+    dynamic queryResult = await onQuery(query, operation: 'SELECT');
+
+    // casting the values if list or map depend on isSinlge or not.
+    List<Map<String, dynamic>> result = [];
+
+    if (isSinlge) {
+      result = [queryResult];
+    } else {
+      result = List<Map<String, dynamic>>.from(queryResult);
+    }
+
     return result.map((e) => toModel(e)).toList();
   }
 }
--- lib/presentation/pages/splash/cubit/splash_cubit.dart
+++ lib/presentation/pages/splash/cubit/splash_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zakio/data/supabase/db_manager.dart';
+import 'package:zakio/data/supabase/table/db_controllers/db_column_value.dart';
+import 'package:zakio/data/supabase/table/db_controllers/db_range_values.dart';
 import 'package:zakio/data/supabase/table/tables/children_table.dart';
 import 'package:zakio/presentation/pages/splash/cubit/state/splash_state.dart';
 
@@ -24,6 +26,14 @@ class SplashCubit extends Cubit<SplashState> {
         ChildrenTableColumn.name,
         // ChildrenTableColumn.createdAt,
       ],
+      isEqual: DBColumnValue(
+        tableColumns: [ChildrenTableColumn.name],
+        value: "Alsaa",
+      ),
+      // orderBy: ChildrenTableColumn.name,
+      // limit: 1,
+      range: DbRangeValues(from: 0, to: 10),
+      isSinlge: true,
     );
     print(children);
   }
